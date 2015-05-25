e7910db4e94c769ccc88de30be2166fe4ca6cadd
Michael Pankov <work@michaelpankov.com>
2015-05-25 15:41:02 +0300

    First building version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate regex;
+
+use std::collections::HashMap;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let re = regex::Regex::new("\"GET (?:https?://.+?/)?(.+?\\.mp3) \
+                                HTTP/1\\.[01]\" \\d{3} (\\d+)").unwrap();
+    let stdin = std::io::stdin();
+    let locked_stdin = stdin.lock();
+    let mut stat: HashMap<String, (isize, isize)> = HashMap::new();
+
+    for maybe_line in locked_stdin.lines() {
+        let line = maybe_line.unwrap();
+        println!("{}", line);
+        let captures: Vec<_> = re.captures_iter(&line).collect();
+        if captures.len() == 1 {
+            let fname = captures[0].at(1).unwrap();
+            let size: isize =
+                std::str::FromStr::from_str(
+                    captures[0].at(2).unwrap())
+                .unwrap();
+            if ! stat.contains_key(fname) {
+                stat.insert(fname.to_owned(), (size, size));
+            } else {
+                let pair = stat.get(fname).unwrap().clone();
+                let (mut sum, mut max) = pair;
+                sum += size;
+                max = std::cmp::max(max, size);
+                stat.insert(fname.to_owned(), (sum, max));
+            }
+        }
+    };
+
+    for it in stat {
+        let (key, value) = it;
+        let (sum, mut max) = value;
+        if max == 0 {
+            max = 1;
+        }
+        let downloads: f64 = sum as f64 / max as f64;
+        println!("Key: {} downloads: {} (max size: {}_)",
+                 key, downloads, max);
+    }
 }
